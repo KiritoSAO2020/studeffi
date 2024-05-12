8092138a10a5928a51bd8a3902b99b94414464d5
Allan Francin <69412135+KiritoSAO2020@users.noreply.github.com>
2024-05-12 20:27:40 +0200

    Update welcome_message.php

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <title>Welcome to CodeIgniter 4! Kiri</title>
-<body>
-<?= view('header') ?>
-    <h1> Main page </h1>
+    <title>Page de bienvenue</title>
+
+<body class="d-flex flex-column min-vh-100">
+    <?= view('header') ?>
+    <div class="container d-flex flex-column justify-content-center align-items-center flex-grow-1">
+        <h1>Veuillez vous connecter afin d'accéder au tableau de bord. </h1>
+    </div>
 </body>
+
 </html>
